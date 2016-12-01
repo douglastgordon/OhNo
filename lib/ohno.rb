c0421267c8136fe60b108ef381fbb510c5f72606
Douglas Gordon <douglastgordon@gmail.com>
2016-11-30 20:09:46 -0500

    make module methods

--- lib/ohno.rb
+++ lib/ohno.rb
@@ -1,34 +1,34 @@
 module OhNo
 
-  def infinite_loop
+  def self.infinite_loop
     until false
     end
   end
 
-  def stack_overflow
+  def self.stack_overflow
     stack_overflow
   end
 
 
-  def divide_by_zero
+  def self.divide_by_zero
     42/0
   end
 
-  def index_error
+  def self.index_error
     small_array = [1, 2, 3]
     big_idx = 1_000_000
     small_array.fetch(big_idx)
   end
 
-  def type_error
+  def self.type_error
     20 / "spaghetti"
   end
 
-  def no_method_error
+  def self.no_method_error
     youknowme!
   end
 
-  def argument_error
+  def self.argument_error
     i_take_one_argument("Here's", "a", "bunch", "of", "arguments")
   end
 
